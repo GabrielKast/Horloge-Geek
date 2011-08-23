e921c45ec241ff912b2149a48530b00d31dcc1dc
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2011-08-24 00:05:53 +0200

    Display the Localtime instead of the UTC time. Format date.

--- colorclock.js
+++ colorclock.js
@@ -1,98 +1,101 @@
-
-  function getPortWidth() {
+function getPortWidth() {
     var viewportwidth;
 
     // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
     if (typeof window.innerWidth != 'undefined')
     {
-      viewportwidth = window.innerWidth;
+	viewportwidth = window.innerWidth;
     }
 
     // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
 
     else if (typeof document.documentElement != 'undefined'
-      && typeof document.documentElement.clientWidth !=
-      'undefined' && document.documentElement.clientWidth != 0)
+	     && typeof document.documentElement.clientWidth !=
+	     'undefined' && document.documentElement.clientWidth != 0)
     {
-      viewportwidth = document.documentElement.clientWidth;
+	viewportwidth = document.documentElement.clientWidth;
     }
 
     // older versions of IE
     else
     {
-      viewportwidth = document.getElementsByTagName('body')[0].clientWidth;
+	viewportwidth = document.getElementsByTagName('body')[0].clientWidth;
     }
 
-     return viewportwidth;
-  }
+    return viewportwidth;
+}
 
-  function getPortHeight() {
+function getPortHeight() {
     var viewportwidth;
 
     // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
     if (typeof window.innerWidth != 'undefined')
     {
-      viewportheight = window.innerHeight;
+	viewportheight = window.innerHeight;
     }
 
     // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
 
     else if (typeof document.documentElement != 'undefined'
-      && typeof document.documentElement.clientWidth !=
-      'undefined' && document.documentElement.clientWidth != 0)
+	     && typeof document.documentElement.clientWidth !=
+	     'undefined' && document.documentElement.clientWidth != 0)
     {
-      viewportheight = document.documentElement.clientWidth;
+	viewportheight = document.documentElement.clientWidth;
     }
 
     // older versions of IE
     else
     {
-      viewportheight = document.getElementsByTagName('body')[0].clientHeight;
+	viewportheight = document.getElementsByTagName('body')[0].clientHeight;
     }
 
-     return viewportheight;
-  }
+    return viewportheight;
+}
 
 
-  function updateTime() {
+function updateTime() {
     var d = new Date();
-    var h = d.getUTCHours();
-    var m = d.getUTCMinutes();
-    var s = d.getUTCSeconds();
-    var ms = d.getUTCMilliseconds();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var ms = d.getMilliseconds();
     var geektime = Math.round(65536 * (3600000 * h + 60000 * m + 1000 * s + ms) / (24 * 60 * 60 * 1000));
-    var padding = "";
-    if (geektime < 0x1000) padding = "0";
-    if (geektime < 0x100) padding = "00";
-    if (geektime < 0x10) padding = "000";
-    geektime = padding + geektime.toString(16).toUpperCase();
+    var formatted_geektime=format_as_a_geek(geektime);
 
-    var yy = d.getUTCFullYear();
-    var mm = d.getUTCMonth();
-    var dd = d.getUTCDate();
+
+    var yy = d.getFullYear();
+    var mm = d.getMonth();
+    var dd = d.getDate();
 
     var geekdate = Math.round((Date.UTC(yy, mm, dd) - Date.UTC(yy, 0, 1)) / (24 * 60 * 60 * 1000));
-    padding = "";
-    if (geekdate < 0x100) padding = "0";
-    if (geekdate < 0x10) padding = "00";
-    geekdate = padding + geekdate.toString(16).toUpperCase();
+    var formatted_geekdate=format_as_a_geek(geekdate);
 
+    var hexall = new String(formatted_geektime+formatted_geekdate);
+    console.log(hexall)
+    var hexcolor = hexall.substr(hexall.length-6,6);
+    var format_to_display=formatted_geekdate+formatted_geektime;
 
-    var hexcolor = new String(geektime+geekdate);
-    hexcolor = hexcolor.substr(hexcolor.length-6,6);
+    var hh = format_number(h), 
+    mm = format_number(m), 
+    ss = format_number(s);
 
-    if (h < 10) h = "0"+h;
-    if (m < 10) m = "0"+m;
-    if (s < 10) s = "0"+s;
-
-    $('#clock').html("0x"+geekdate+geektime+"<br /><small>"+h+":"+m+":"+s+"</small>");
+    $('#clock').html("0x"+format_to_display+"<br /><small>"+hh+":"+mm+":"+ss+"</small>");
+    // hexall instead of hexcolor
     $('body').css('background-color', "#"+hexcolor);
+}
+function format_as_a_geek(geekthing) {
+    if (geekthing < 0x1000) padding = "0";
+    if (geekthing < 0x100) padding = "00";
+    if (geekthing < 0x10) padding = "000";
+    return padding + geekthing.toString(16).toUpperCase();
+}
+function format_number(n) {
+    return  (n < 10)?"0"+n:n;
+}
 
 
-  }
-
-  $(document).ready(function() {
+$(document).ready(function() {
     updateTime();
     setInterval("updateTime()", 650);
-  });
+});
 
